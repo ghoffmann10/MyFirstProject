ce6ae791607d27b72a39f2e427254cc807922597
ghoffmann10 <hoffmann10@iccc.cc.ia.us>
2019-08-22 14:24:12 -0500

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,20 @@
+/*
+Grace Hoffmann
+C++ Fall 2019
+Due: August 29, 2019
+Lab 1 Introduction to C++ Programing
+This lab will serve as a demonstration of your ability to download a project
+from GitHub, compile and run the project, upload a project to GitHub, and submit a
+project to Google Classroom.
+*/
+
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "HelloWorld! purple"; 
+
+	return 0;
+}
